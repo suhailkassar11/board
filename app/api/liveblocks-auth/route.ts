63560afc41b2e78cf4c21094f5bc3df83870e8e7
suhailKassar11 <suhail.1923co1011@kiet.edu>
2024-03-24 14:19:33 +0530

    add secret key

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,7 +6,7 @@ import { auth, currentUser } from "@clerk/nextjs";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 const liveblocks = new Liveblocks({
-  secret: "sk_dev_deZy9AxvNtaPq5dADx3vm43LFRr_mCkGUOh8ZVf2kJGJ6JNkbl8PPMws1gmgYWNs",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!
 });
 
 export const POST=async(request:Request)=>{
